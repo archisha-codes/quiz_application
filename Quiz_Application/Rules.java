fc24bb0bc7d13e8125dcc5e88b8a13eee7f3ce6d
Archisha Ranjan <archisha.git@gmail.com>
2024-10-29 00:22:35 +0530

    Add files via upload

---
+++ Quiz_Application/Rules.java
@@ -0,0 +1,69 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Rules extends JFrame implements ActionListener{
+
+    String name;
+    JButton start, back;
+    
+    Rules(String name) {
+        this.name = name;
+        getContentPane().setBackground(Color.WHITE);
+        setLayout(null);
+        
+        JLabel heading = new JLabel("Welcome " + name + " to Question street");
+        heading.setBounds(50, 20, 700, 30);
+        heading.setFont(new Font("Viner Hand ITC", Font.BOLD, 28));
+        heading.setForeground(new Color(30, 144, 254));
+        add(heading);
+        
+        JLabel rules = new JLabel();
+        rules.setBounds(20, 90, 700, 350);
+        rules.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        rules.setText(
+            "<html>"+ 
+                "1. No other electronics allowed" + "<br><br>" +
+                "2. Clear environment: the surface where your device is placed must be entirely clear of anything other than the test device. Books, papers, electronics, and any other objects are not allowed" + "<br><br>" +
+                "3. Take the exam independently" + "<br><br>" +
+                "4. No communication: no communicating with other individuals, whether verbal, non-verbal, or electronic." + "<br><br>" +
+                "5. Well-lit environment, clear webcam view" + "<br><br>" +
+                "6. Stay on camera: remain fully visible on the webcam throughout the test" + "<br><br>" +
+                "7. One screen only: only one screen is permitted during the test; using any other screens, monitors, or display devices is strictly prohibited" + "<br><br>"  +
+            "<html>"
+        );
+        add(rules);
+        
+        back = new JButton("Back");
+        back.setBounds(250, 500, 100, 30);
+        back.setBackground(new Color(30, 144, 254));
+        back.setForeground(Color.WHITE);
+        back.addActionListener(this);
+        add(back);
+        
+        start = new JButton("Start");
+        start.setBounds(400, 500, 100, 30);
+        start.setBackground(new Color(30, 144, 254));
+        start.setForeground(Color.WHITE);
+        start.addActionListener(this);
+        add(start);
+        
+        setSize(800, 650);
+        setLocation(350, 100);
+        setVisible(true);
+    }
+    
+    public void actionPerformed(ActionEvent ae) {
+        if (ae.getSource() == start) {
+            setVisible(false);
+            new Quiz(name);
+        } else {
+            setVisible(false);
+            new Login();
+        }
+    }
+    
+    public static void main(String[] args) {
+        new Rules("User");
+    }
+}
